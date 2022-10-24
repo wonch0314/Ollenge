df9bbcdc58032bae8a8e4039af83f336b30f5140
SieunWoo <ramtk6726@naver.com>
2022-10-24 16:18:11 +0900

    [[#S07P31A501-111] feat: Add entity report

--- backend/src/main/java/com/ollenge/db/entity/User.java
+++ backend/src/main/java/com/ollenge/db/entity/User.java
@@ -1,12 +1,10 @@
 package com.ollenge.db.entity;
 
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,7 +12,6 @@ import java.time.LocalDateTime;
 @Entity
 @Builder
 @AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
