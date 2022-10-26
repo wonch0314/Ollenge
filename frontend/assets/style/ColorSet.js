d4563c1ad492b931dd83102a79fa353cb3d85745
Hyelim <hallov012@gmail.com>
2022-10-27 01:29:32 +0900

    [S07P31A501-133] design: add ColorSet.js

---
+++ frontend/assets/style/ColorSet.js
@@ -0,0 +1,7 @@
+export default {
+  orangeColor: (opacity) => `rgba(248, 132, 64, ${opacity})`,
+  navyColor: (opacity) => `rgba(0, 78, 102, ${opacity})`,
+  yellowColor: (opacity) => `rgba(252, 191, 50, ${opacity})`,
+  paleBlueColor: (opacity) => `rgba(237, 248, 255, ${opacity})`,
+  greenColor: (opacity) => `rgba(85, 238, 154, ${opacity})`,
+};
