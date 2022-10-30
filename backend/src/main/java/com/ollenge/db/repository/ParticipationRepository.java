2ac0b1774e5fe569f08c51e994a290283b62bc08
WOOSIEUN <ramtk6726@naver.com>
2022-10-30 19:23:37 +0900

    [S07P31A501-115] feat: Add user and challenge data to participation table when user creates new challenge

---
+++ backend/src/main/java/com/ollenge/db/repository/ParticipationRepository.java
@@ -0,0 +1,8 @@
+package com.ollenge.db.repository;
+
+import com.ollenge.db.entity.ClassificationType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassificationTypeRepository extends JpaRepository<ClassificationType, Long> {
+
+}
