dd272cf7aa84b137236836be44a78b384d5cd18e
bizyoung93 <bizyoung93@gmail.com>
2022-10-31 11:10:58 +0900

    MyCGScreen 중간저장 + AppCard 컴포넌트 생성 + AppText Bold까지 지정하도록

--- frontend/components/MyCGScreen/ChallengingCard.js
+++ frontend/components/MyCGScreen/ChallengingCard.js
@@ -2,33 +2,12 @@ import React from "react-native"
 import { View, Text, Dimensions } from "react-native"
 import ColorSet from "../../style/ColorSet"
 import AppText from "../common/AppText"
-import { Shadow } from "react-native-shadow-2"
+import AppCard from "../common/AppCard"
 
 export default function ChallengingCard() {
-  const windowWidth = Dimensions.get("window").width
   return (
-    // 공간
-    <View
-      style={{
-        backgroundColor: "#edf8ff",
-        height: 170,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {/* 카드 */}
-      <Shadow>
-        <View
-          style={{
-            width: windowWidth * 0.9,
-            height: 150,
-            backgroundColor: "white",
-            borderRadius: 10,
-          }}
-        >
-          {/* <Text style={{ margin: 20, fontSize: 20 }}>🙂🙂🙂</Text> */}
-        </View>
-      </Shadow>
-    </View>
+    <AppCard height="170">
+      <AppText>dsfs</AppText>
+    </AppCard>
   )
 }
