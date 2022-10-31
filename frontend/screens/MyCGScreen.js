692401b601ed2b25c1614e213cb340a090eab26e
bizyoung93 <bizyoung93@gmail.com>
2022-10-31 12:13:51 +0900

    [S07P31A501-212]중간저장 + AppCard 컴포넌트 생성 + AppText Bold까지 지정하도록

--- frontend/screens/MyCGScreen.js
+++ frontend/screens/MyCGScreen.js
@@ -1,13 +1,79 @@
-import { View, StyleSheet } from "react-native";
+import React from "react-native"
+import styled from "styled-components"
+import AppText from "../components/common/AppText"
+import { NavigationContainer } from "@react-navigation/native"
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
+import BeforeStart from "../components/MyCGScreen/BeforeStart"
+import Challenging from "../components/MyCGScreen/Challenging"
+import Ended from "../components/MyCGScreen/Ended"
+import ColorSet from "../style/ColorSet"
 
 function MyCGScreen() {
-  return <View style={styles.rootScreen}></View>;
-}
-export default MyCGScreen;
+  const Tab = createMaterialTopTabNavigator()
 
-const styles = StyleSheet.create({
-  rootScreen: {
-    flex: 1,
-    backgroundColor: "#000000",
-  },
-});
+  return (
+    <Body>
+      {/* Header부분 */}
+      <Header>
+        <HeaderTextView>
+          <HeaderTextColumn>
+            <AppText size="28" weight="900">
+              내 챌린지
+            </AppText>
+          </HeaderTextColumn>
+          <HeaderTextColumn></HeaderTextColumn>
+        </HeaderTextView>
+      </Header>
+      <NavigationContainer>
+        <Tab.Navigator
+          style={{
+            flex: 8,
+          }}
+          screenOptions={{
+            tabBarLabelStyle: { fontSize: 16, fontWeight: "900" },
+            // 글자색
+            tabBarActiveTintColor: `${ColorSet.orangeColor(1)}`,
+            tabBarInactiveTintColor: `${ColorSet.navyColor(0.5)}`,
+            // 언더바색 => 대체 왜 이걸 이렇게 짜놨지?
+            tabBarIndicatorStyle: {
+              backgroundColor: `${ColorSet.orangeColor(1)}`,
+            },
+          }}
+        >
+          <Tab.Screen name="도전 중" component={Challenging} />
+          <Tab.Screen name="시작 전" component={BeforeStart} />
+          <Tab.Screen name="종료" component={Ended} />
+        </Tab.Navigator>
+      </NavigationContainer>
+      {/* 아래부분 */}
+    </Body>
+  )
+}
+
+export default MyCGScreen
+
+const Body = styled.View`
+  flex: 1;
+  background-color: white;
+  width: 100%;
+`
+
+// styled-components 부분
+
+// Header
+const Header = styled.View`
+  flex: 1.5;
+  background-color: white;
+`
+
+const HeaderTextView = styled.View`
+  flex-direction: row;
+  flex: 6;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 10px 23px;
+`
+
+const HeaderTextColumn = styled.View`
+  flex: 1;
+`
