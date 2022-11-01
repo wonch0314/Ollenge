00d71a9fde34231612499e426459fd333748aa9b
SieunWoo <ramtk6726@naver.com>
2022-11-01 17:29:46 +0900

    [S07P31A501-223] feat: Hide kakao login api key

--- backend/src/main/java/com/ollenge/api/service/OAuthService.java
+++ backend/src/main/java/com/ollenge/api/service/OAuthService.java
@@ -18,6 +18,9 @@ import java.net.URL;
 @Service
 public class OAuthService {
 
+    @Value("${kakao.apikey}")
+    private String apiKey;
+
     @Autowired
     UserRepository userRepository;
 
@@ -37,7 +40,7 @@ public class OAuthService {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
-            sb.append("&client_id="+"e92257cb82c9cbdfea8773e3656f72a2");
+            sb.append("&client_id="+apiKey);
             sb.append("&redirect_uri=http://localhost:8080/api/oauth/kakao");
             sb.append("&code=" + code);
             bw.write(sb.toString());
