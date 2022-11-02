915ac082b53c14cf0ba240577e4f0b53344663bc
Cr-Mo-Marco-3000 <bizyoung93@gmail.com>
2022-11-03 00:25:37 +0900

    [S07P31A501-218] AppBoldText와 AppText의 사이즈 props을 넘겨주는 로직을 통일했습니다 - 누락된 부분 수정

--- frontend/components/common/AppBoldText.js
+++ frontend/components/common/AppBoldText.js
@@ -6,7 +6,7 @@ import { RFPercentage } from "react-native-responsive-fontsize"
 
 /*
 
-Text 컴포넌트
+AppBoldText
 
 - props 설명
 
@@ -24,17 +24,28 @@ RFPercentage에서 쓸 숫자를 넣습니다
 
 Caution!!! 
 만약 size와 pxSize가 둘 다 들어갔다면, 우선권은 size에 있습니다.
+둘 다 들어가지 않았다면, RFPercentage(3)이 우선 적용됩니다.
 둘 중 하나를 취사선택해서 쓰시기 바랍니다.
+
+props를 넘겨줄 때의 형식은 아래와 같습니다.
+size={3}
+pxSize={24}
 
 */
 const AppBoldText = (props) => {
   const selectedColor = props.color ? props.color : "navy"
 
-  const RFP = props.size ? RFPercentage(props.size) : RFPercentage(3)
+  let selectedSize = 24
 
-  const pxSize = props.pxSize ? props.pxSize : 24
-
-  const selectedSize = props.size ? RFP : pxSize
+  if (!props.size && !props.pxSize) {
+    selectedSize = RFPercentage(3)
+  } else if (props.size && !props.pxSize) {
+    selectedSize = RFPercentage(props.size)
+  } else if (!props.size && props.pxSize) {
+    selectedSize = props.pxSize
+  } else if (props.size && props.pxSize) {
+    selectedSize = RFPercentage(props.size)
+  }
 
   const theme = {
     orange: ColorSet.orangeColor(1),
