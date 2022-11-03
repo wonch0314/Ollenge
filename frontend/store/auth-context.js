bc5145b9df7abb57a8266cfc4ff43f70fad78870
hallov012 <hallov012@gmail.com>
2022-11-03 17:51:11 +0900

    upload

--- frontend/store/auth-context.js
+++ frontend/store/auth-context.js
@@ -1,19 +1,37 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import { createContext, useState } from "react"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 export const AuthContext = createContext({
   token: "",
   isAuthenticated: false,
+  isSigned: false,
   authenticate: (token) => {},
+  signed: (flag) => {},
   logout: () => {},
 })
 
 function AuthContextProvider({ children }) {
   const [authToken, setAuthToken] = useState()
+  const [userFalg, setUserFlag] = useState()
+
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem("token")
+      if (storedToken) {
+        setAuthToken(storedToken)
+      }
+    }
+  }, [])
 
   function authenticate(token) {
     setAuthToken(token)
+    AsyncStorage.setItem("token", token)
+  }
+
+  function signed(flag) {
+    setUserFlag(flag)
   }
 
   function logout() {
@@ -23,7 +41,9 @@ function AuthContextProvider({ children }) {
   const value = {
     token: authToken,
     isAuthenticated: !!authToken,
+    isSigned: userFalg,
     authenticate: authenticate,
+    signed: signed,
     logout: logout,
   }
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
