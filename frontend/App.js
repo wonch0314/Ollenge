627363f4e21db13cd908b6a3455ac27877e39e4f
bizyoung93 <bizyoung93@gmail.com>
2022-11-04 15:07:13 +0900

    [S07P31A501-218] RankingCGScreen 템플릿 완료

--- frontend/App.js
+++ frontend/App.js
@@ -32,7 +32,7 @@ const App = () => {
     })
   }
 
-  const [index, setIndex] = React.useState(4)
+  const [index, setIndex] = React.useState(1)
   const [routes] = React.useState([
     {
       key: "myCG",
--- frontend/src/assets/images/RankingCGScreen/RankingCGScreen.js
+++ frontend/src/assets/images/RankingCGScreen/RankingCGScreen.js
@@ -6,7 +6,16 @@ import Notebook from "./notebook-icon.png"
 import Sun from "./sun-icon.png"
 import Boy from "./boy-icon.png"
 
-export function GymIcon() {
+import Calendar from "./calendar_icon.png"
+import GreenSquare from "./green_square_icon.png"
+import Hand from "./hand_icon.png"
+import OrangeSquare from "./orange_square_icon.png"
+import Stopwatch from "./stopwatch_icon.png"
+
+export function GymIcon(props) {
+  const left = props.left ? props.left : "-15%"
+  const top = props.top ? props.top : "-15%"
+
   return (
     <Image
       source={Gym}
@@ -16,14 +25,16 @@ export function GymIcon() {
         width: "160%",
         opacity: 0.5,
         transform: [{ rotate: "-54.67deg" }],
-        left: "-15%",
-        top: "-15%",
+        left: left,
+        top: top,
       }}
       resizeMode="contain"
     />
   )
 }
-export function NotebookIcon() {
+export function NotebookIcon(props) {
+  const left = props.left ? props.left : "5%"
+  const top = props.top ? props.top : 0
   return (
     <Image
       source={Notebook}
@@ -32,14 +43,17 @@ export function NotebookIcon() {
         width: "110%",
         height: "110%",
         position: "absolute",
-        bottom: "-12%",
-        left: "5%",
+        top: top,
+        left: left,
       }}
       resizeMode="contain"
     />
   )
 }
-export function SunIcon() {
+export function SunIcon(props) {
+  const left = props.left ? props.left : "-14%"
+  const top = props.top ? props.top : "-15%"
+
   return (
     <Image
       source={Sun}
@@ -48,8 +62,8 @@ export function SunIcon() {
         width: "150%",
         height: "150%",
         position: "absolute",
-        left: "-14%",
-        top: "-15%",
+        left: left,
+        top: top,
       }}
       resizeMode="contain"
     />
@@ -67,4 +81,24 @@ export function BoyIcon() {
       resizeMode="contain"
     />
   )
+}
+
+export function CalendarIcon() {
+  return <Image source={Calendar} style={{ height: "100%", width: "100%" }} resizeMode="contain" />
+}
+export function GreenSquareIcon() {
+  return (
+    <Image source={GreenSquare} style={{ height: "100%", width: "100%" }} resizeMode="contain" />
+  )
+}
+export function HandIcon() {
+  return <Image source={Hand} style={{ height: "100%", width: "100%" }} resizeMode="contain" />
+}
+export function OrangeSquareIcon() {
+  return (
+    <Image source={OrangeSquare} style={{ height: "100%", width: "100%" }} resizeMode="contain" />
+  )
+}
+export function StopwatchIcon() {
+  return <Image source={Stopwatch} style={{ height: "100%", width: "100%" }} resizeMode="contain" />
 }
