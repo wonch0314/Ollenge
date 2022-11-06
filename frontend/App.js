8ed88a029674220c8d0d09681882976cd1a8870b
Hyelim <hallov012@gmail.com>
2022-11-06 20:48:28 +0900

    [S07P31A501-192] logout

--- frontend/App.js
+++ frontend/App.js
@@ -89,6 +89,8 @@ function Navigation() {
   useEffect(() => {
     if (authCtx.isAuthenticated && authCtx.isSigned) {
       setAuthFlag(true)
+    } else {
+      setAuthFlag(false)
     }
   }, [authCtx])
   return (
@@ -128,7 +130,7 @@ function Root() {
     fetchToken()
     fetchSigned()
     getFonts()
-  }, [])
+  }, [authCtx])
 
   if (isTryingLogin) {
     return <AppLoading />
@@ -145,96 +147,6 @@ function App() {
     </>
   )
 }
-
-// const MyCGRoute = () => <MyCGScreen />
-
-// const RankingCGRoute = () => <RankingCGScreen />
-
-// const UserRankRoute = () => <UserRankScreen />
-
-// const MyPageRoute = () => <MyPageScreen />
-
-// const CreateCGRoute = () => <CreateCGScreen />
-
-// const App = () => {
-//   const [isReady, setIsReady] = useState(false)
-//   const getFonts = async () => {
-//     await Font.loadAsync({
-//       Recipekorea: require("./src/assets/fonts/Recipekorea.ttf"),
-//       HyeminRegular: require("./src/assets/fonts/HyeminRegular.ttf"),
-//       HyeminBold: require("./src/assets/fonts/HyeminBold.ttf"),
-//     })
-//   }
-
-//   const [index, setIndex] = React.useState(4)
-//   const [routes] = React.useState([
-//     {
-//       key: "myCG",
-//       title: "내 챌린지",
-//       focusedIcon: "home",
-//     },
-//     { key: "rankingCG", title: "랭킹 챌린지", focusedIcon: "fire" },
-//     { key: "userRank", title: "유저랭킹", focusedIcon: "crown" },
-//     {
-//       key: "myPage",
-//       title: "마이페이지",
-//       focusedIcon: "account",
-//     },
-//     { key: "createCG", title: "챌린지 생성", focusedIcon: "crown" },
-//   ])
-
-//   const renderScene = BottomNavigation.SceneMap({
-//     myCG: MyCGRoute,
-//     rankingCG: RankingCGRoute,
-//     userRank: UserRankRoute,
-//     myPage: MyPageRoute,
-//     createCG: CreateCGRoute,
-//   })
-
-//   function startScreenChange() {
-//     setIndex(0)
-//   }
-//   // index == 4인 경우 StartScreen 출력, 그 외엔 BottomNav랑 해당 스크린 출력
-
-//   const authCtx = useContext(AuthContext)
-//   console.log(authCtx.isAuthenticated)
-//   return (
-//     <AuthContextProvider>
-//       {isReady ? (
-//         <View style={styles.rootScreen}>
-//           {index == 4 ? (
-//             <StartScreen startScreenChange={startScreenChange} />
-//           ) : (
-//             <BottomNavigation
-//               navigationState={{ index, routes }}
-//               onIndexChange={setIndex}
-//               renderScene={renderScene}
-//               compact={false}
-//               sceneAnimationType={"shifting"}
-//               barStyle={styles.bottomNavContainer}
-//               activeColor={`${ColorSet.orangeColor(1)}`}
-//               inactiveColor={`${ColorSet.navyColor(1)}`}
-//               theme={{
-//                 fonts: {
-//                   labelMedium: {
-//                     fontFamily: "HyeminBold",
-//                   },
-//                 },
-//                 colors: {
-//                   onSurfaceVariant: `${ColorSet.navyColor(1)}`,
-//                   onSurface: `${ColorSet.orangeColor(1)}`,
-//                   secondaryContainer: "#FF999900",
-//                 },
-//               }}
-//             />
-//           )}
-//         </View>
-//       ) : (
-//         <AppLoading startAsync={getFonts} onFinish={() => setIsReady(true)} onError={() => {}} />
-//       )}
-//     </AuthContextProvider>
-//   )
-// }
 
 export default App
 
