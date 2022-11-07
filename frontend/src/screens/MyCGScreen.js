71cd365437395a89659dd61a695f5c2e61bf1c33
hallov012 <hallov012@gmail.com>
2022-11-07 13:58:16 +0900

    [S07P31A501-246] feat: CG Room navigation

--- frontend/src/screens/MyCGScreen.js
+++ frontend/src/screens/MyCGScreen.js
@@ -5,6 +5,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import MyCGListScreen from "./MyCGListScreen"
 import CGRoomScreen from "./CGRoomScreen"
+import CGUserScreen from "../components/CGRoomScreen.js/CGUserScreen"
+import AuthScreen from "../components/CGRoomScreen.js/AuthScreen"
+import ImageRegisterPage from "../components/CGRoomScreen.js/ImageRegisterScreen"
+
 import ColorSet from "../style/ColorSet"
 
 const Stack = createNativeStackNavigator()
@@ -22,6 +26,9 @@ function MyCGScreen() {
       >
         <Stack.Screen name="CGList" options={{ headerShown: false }} component={MyCGListScreen} />
         <Stack.Screen name="CGRoom" component={CGRoomScreen} />
+        <Stack.Screen name="CGUser" component={CGUserScreen} />
+        <Stack.Screen name="CGAuth" component={AuthScreen} />
+        <Stack.Screen name="CGImg" component={ImageRegisterPage} />
       </Stack.Navigator>
     </NavigationContainer>
   )
