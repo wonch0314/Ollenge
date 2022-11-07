6dde1dd62577206000ddd08ac6ed801eb3efd09a
dheldh20 <dheldh20@gmail.com>
2022-11-07 18:00:29 +0900

    [S07P31A501-266]Feature:anything

--- frontend/src/screens/CGRoomScreen.js
+++ frontend/src/screens/CGRoomScreen.js
@@ -16,10 +16,16 @@ function CGRoomScreen() {
         <AppButton title={"유저목록"} handler={() => navigation.push("CGUser")}></AppButton>
       </View>
       <View style={{ height: 50 }}>
-        <AppButton title={"인증이미지등록"} handler={() => navigation.push("CGImg")}></AppButton>
+        <AppButton
+          title={"인증이미지등록"}
+          handler={() => navigation.push("CGImg", { methodNum: 0, participationId: 3 })}
+        ></AppButton>
       </View>
       <View style={{ height: 50 }}>
-        <AppButton title={"인증"} handler={() => navigation.push("CGAuth")}></AppButton>
+        <AppButton
+          title={"인증"}
+          handler={() => navigation.push("CGAuth", { methodNum: 1, participationId: 3 })}
+        ></AppButton>
       </View>
     </View>
   )
