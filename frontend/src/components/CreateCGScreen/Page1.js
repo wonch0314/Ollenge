76b1067b92ec46feb7d15dddfb9649122513c5ee
원찬호 <chwon03@naver.com>
2022-11-10 13:04:19 +0900

    [S07P31A501-283] update: Page1 Design

--- frontend/src/components/CreateCGScreen/Page1.js
+++ frontend/src/components/CreateCGScreen/Page1.js
@@ -3,7 +3,7 @@ import { KeyboardAvoidingView, Text, View } from "react-native"
 import PageBase, { fontStyles } from "./PageBase"
 import ImagePicker from "../common/ImagePicker"
 import TextInputContainer from "../common/TextInputContainer"
-
+import { dw, dh } from "../../style/DeviceInfo"
 export default function Page1({ info, setInfo }) {
   const [name, setName] = useState(info.challengeName)
   const [img, setImg] = useState(info.challengeImg)
@@ -17,19 +17,20 @@ export default function Page1({ info, setInfo }) {
   }, [name, setName, AwsUrl, setAwsUrl])
 
   useEffect(() => {
+    console.log(img)
     setDisabled(name === "")
   }, [name, img, disabled, setDisabled])
 
   return (
     <PageBase toNext={"Page2"} disabled={disabled}>
       <View style={{ width: "100%", flex: 1 }}>
-        <ImagePicker imageUri={img} imageUriHandler={setImg} imageBase64Handler={setAwsUrl} />
+        <View style={{ flex: 1, marginBottom: dh * 0.1 }}>
+          <ImagePicker imageUri={img} imageUriHandler={setImg} imageBase64Handler={setAwsUrl} />
+        </View>
         <View style={{ width: "100%", flex: 1 }}>
           <Text style={frameStyles.inputArea}>팀 이름</Text>
           <View style={{ width: "100%" }}>
-            <View style={{ height: 30 }}>
-              <TextInputContainer inputText={name} inputHandler={setName} />
-            </View>
+            <TextInputContainer inputText={name} inputHandler={setName} />
           </View>
         </View>
       </View>
