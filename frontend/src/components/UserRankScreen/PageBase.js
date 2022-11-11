fc74edd8ae6a7bfb5d4b2d6e68df7fab81bcbfd7
원찬호 <chwon03@naver.com>
2022-11-11 14:31:52 +0900

    [S07P31A501-279] add: UserRank API 연동 UserRank에서 데이터를 받아오는 API코드 입력 완료

---
+++ frontend/src/components/UserRankScreen/PageBase.js
@@ -0,0 +1,57 @@
+/** react-native 시스템 Import */
+import React from "react"
+import { Keyboard, TouchableWithoutFeedback } from "react-native"
+import { LinearGradient } from "expo-linear-gradient"
+
+/** Component Import */
+import ColorSet from "../../style/ColorSet"
+
+/** 페이지에서 사용할 스타일들 정리 */
+import { StyleSheet } from "react-native"
+import DeviceInfo from "../../style/DeviceInfo"
+import TopMargin from "../common/TopMargin"
+
+const { dw } = DeviceInfo
+
+/** 모든 페이지의 기본 배경 설정 */
+export default function PageBase(props) {
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <LinearGradient
+        style={{ flex: 1, width: "100%" }}
+        colors={[`${ColorSet.paleBlueColor(1)}`, `${ColorSet.yellowColor(1)}`]}
+        end={{ x: 0, y: 1 }}
+        locations={[0.8, 1]}
+      >
+        <TopMargin />
+        {props.children}
+      </LinearGradient>
+    </TouchableWithoutFeedback>
+  )
+}
+
+// export const fontStyles = StyleSheet.create({
+//   HyeminBold: (props) => {
+//     return {
+//       fontFamily: "HyeminBold",
+//       fontSize: props.size !== undefined ? (props.size * dw) / 100 : 24,
+//       fontWeight: props.bold !== undefined ? props.bold : "100",
+//       color: props.color !== undefined ? props.color : ColorSet.navyColor(1),
+//       textAlign: props.align ? props.align : "center",
+//     }
+//   },
+
+//   Hyemin: (props) => {
+//     return {
+//       fontFamily: "HyeminRegular",
+//       fontSize: props.size !== undefined ? (props.size * dw) / 100 : 24,
+//       fontWeight: props.bold !== undefined ? props.bold : "100",
+//       color: props.color !== undefined ? props.color : ColorSet.navyColor(1),
+//       textAlign: props.align ? props.align : "center",
+//     }
+//   },
+// })
+
+const baseStyle = { width: "100%", justifyContent: "center", alignItems: "center" }
+
+const styles = {}
--- frontend/src/components/UserRankScreen/TopUserArea.js
+++ frontend/src/components/UserRankScreen/TopUserArea.js
@@ -1,89 +1,51 @@
 import React from "react"
 import Styled from "styled-components/native"
-import { Dimensions, Image } from "react-native"
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native"
 import { Avatar } from "react-native-paper"
 import AppText from "../common/AppText"
 
-const dw = Dimensions.get("window").width
-const dh = Dimensions.get("window").height
-
-const WholeFrame = Styled.View`
-  width: 100%;
-  height: ${dh * 0.45}px;
-  position : relative;
-  margin-bottom: 24px;
-  margin-top: 48px;
-`
-
-const cardWidth = dw * 0.37
-const cardHeight = (cardWidth * 4) / 3
-
-const FirstCard = Styled.View`
-  width: ${cardWidth}px;
-  z-index: 30
-  position: absolute;
-  left: 31%;
-  top: -5%;
-  align-items: center;
-`
-
-const SecondCard = Styled.View`
-  width: ${cardWidth}px;
-  position: absolute;
-  bottom: 10%;
-  align-items: center;
-`
-
-const ThirdCard = Styled.View`
-  width: ${cardWidth}px;
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  align-items: center;
-`
-
-const BdImage = Styled.View`
-  width: ${cardWidth * 0.9}px;
-  height: ${cardWidth * 0.9}px;
-  overflow: hidden;
-  border: 4px solid ${(props) => (props.color ? props.color : "black")};
-  border-radius: 120px;
-`
-
-export default function TopUserArea() {
+export default function TopUserArea({ topUsers }) {
   return (
-    <WholeFrame>
-      <FirstCard>
-        <Image source={require("../../assets/images/crown.png")} />
-        <BdImage color="gold">
-          <Image
-            source={require("../../assets/profile/me1.jpg")}
-            style={{ width: "100%", height: "100%", borderRadius: 25 }}
-          />
-        </BdImage>
-        <AppText size={4}>Chan</AppText>
-        <AppText size={3}>{(1235).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}점</AppText>
-      </FirstCard>
-      <SecondCard>
-        <BdImage color="silver">
-          <Image
-            source={require("../../assets/profile/me2.jpg")}
-            style={{ width: "100%", height: "100%", borderRadius: 25 }}
-          />
-        </BdImage>
-        <AppText size={4}>Chan</AppText>
-        <AppText size={3}>{(123551).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}점</AppText>
-      </SecondCard>
-      <ThirdCard>
-        <BdImage color="brown">
-          <Image
-            source={require("../../assets/profile/me3.jpg")}
-            style={{ width: "100%", height: "100%", borderRadius: 25 }}
-          />
-        </BdImage>
-        <AppText size={4}>Chan</AppText>
-        <AppText size={3}>{(123551).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}점</AppText>
-      </ThirdCard>
-    </WholeFrame>
+    <View style={frameStyles.wholeFrame}>
+      {[1, 2, 3].map((num) => {
+        return (
+          <View key={num} style={frameStyles.topPlayer(num)}>
+            <Text>asdas</Text>
+            <Text>asdas</Text>
+          </View>
+        )
+      })}
+      {topUsers.map((user, index) => {
+        return (
+          <View key={index}>
+            <Text>
+              {user.nickname}|{index}
+            </Text>
+          </View>
+        )
+      })}
+    </View>
   )
 }
+const common = {
+  borderWidth: 2,
+  justifyContent: "center",
+  alignItems: "center",
+  position: "absolute",
+}
+
+const frameStyles = StyleSheet.create({
+  wholeFrame: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  topPlayer: (number) => {
+    return {
+      ...common,
+      width: ["40%", "35%", "30%"][number - 1],
+      paddingTop: [11, 0, -11][number - 1],
+    }
+  },
+})
