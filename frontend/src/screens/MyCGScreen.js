5a89a4778da827c9cf9cbdca0cdf325ea9f87426
Hyelim <hallov012@gmail.com>
2022-11-12 20:14:06 +0900

    [S07P31A501-248] feat: auth screen template

--- frontend/src/screens/MyCGScreen.js
+++ frontend/src/screens/MyCGScreen.js
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import MyCGListScreen from "./MyCGListScreen"
 import CGRoomScreen from "./CGRoomScreen"
 import CGUserScreen from "../components/CGRoomScreen/CGUserScreen/CGUserScreen"
-import AuthScreen from "../components/CGRoomScreen/AuthScreen"
+import AuthScreen from "../components/AuthScreen/AuthScreen"
 import ResistScreen from "../components/RistisScreen/ResistScreen"
 import { RoomContext } from "../../store/room-context"
 import CreateCGScreen from "../screens/CreateCGScreen"
@@ -23,7 +23,6 @@ function MyCGScreen() {
         headerTintColor: `${ColorSet.navyColor(100)}`,
         headerTitleAlign: "center",
         headerTitleStyle: { fontFamily: "HyeminBold" },
-        headerShown: false,
       }}
     >
       <Stack.Screen name="CGList" options={{ headerShown: false }}>
@@ -36,7 +35,7 @@ function MyCGScreen() {
       <Stack.Screen name="CGUser" options={{ title: "" }}>
         {() => <CGUserScreen />}
       </Stack.Screen>
-      <Stack.Screen name="CGAuth" component={AuthScreen} options={{ title: "" }} />
+      <Stack.Screen name="CGAuth" component={AuthScreen} options={{ title: "오늘 챌린지 인증" }} />
       <Stack.Screen name="CGImg" component={ResistScreen} options={{ title: "인증 이미지 입력" }} />
       <Stack.Screen name="CGCreate" options={{ title: "" }}>
         {() => <CreateCGScreen isRank={false} />}
